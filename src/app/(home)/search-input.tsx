772fbbe9a7751693aa90e66c055dea49be2eeb52
Monsieur-Chat <adrien.blanchet@protonmail.com>
2025-01-31 11:45:20 +0100

    Chapitre 16

--- src/app/(home)/search-input.tsx
+++ src/app/(home)/search-input.tsx
@@ -16,7 +16,6 @@ export const SearchInput = () => {
     <div className="flex-1 flex items-center justify-center">
       <form className="relative max-w-[720px] w-full">
         <Input
-          value={}
           placeholder="Search"
           className="md:text-base placeholder:text-neutral-800 px-14 w-full border-none
           focus-visible:shadow-[0_1px_1px_0_rgba(0,0,0,0.1), 0_1px_3px_1px_rgba(0,0,0,0.1)] rounded-full h-[48px] focus-visible:ring-0 focus:bg-white"
